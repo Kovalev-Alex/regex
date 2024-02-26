4f73b8fe32b8b736f0b0fd776e0e780d7f277830
Alexandr Kovalev <alex_kov@inbox.ru>
2024-02-26 21:52:39 +0500

    crteate def write

--- main.py
+++ main.py
@@ -13,27 +13,45 @@ def insert_to_contact_list():
         raw_names = item[0], item[1], item[2]
         name = ', '.join(raw_names).replace(',', '').split(" ")[:3]
         item[0], item[1], item[2] = name[0], name[1], name[2]
-        data_dict = {
+
+        phonebook[name[0], name[1], name[2]] = {
             'organization': item[3],
             'position': item[4],
             'phone': format_phone_number(item[5]),
             'email': item[6]
         }
-        key = name[0], name[1], name[2]
-        phonebook[key] = data_dict
-
     return phonebook
+# new_contact = [y if x="" else x for x,y in zip(contact_1, contact_2)]
 
 
 def format_phone_number(phone):
     pattern = r"\+?([7|8])\s?\(?(\d{3})\)?[\s-]?(\d{3,})\-?(\d{2,})\-?(\d{2,})\s?\(?(\w{3})?\.?\s?(\d{4})?\)?"
-    new_format = re.sub(pattern, r"+7(\2) \3-\4-\5 \6 \7", phone)
+    new_format = re.sub(pattern, r"+7 (\2) \3-\4-\5 \6 \7", phone)
     return new_format
 
 
+def create_list(list_):
+    phonebook_new = []
+    unique_list = set()
+    for k, v in list_.items():
+        name = k[:2]
+        person = []
+        if name not in unique_list:
+            unique_list.add(name)
+            person.append(', '.join(k[:3]).strip())
+            person.append(v['organization'].strip())
+            person.append(v['position'].strip())
+            person.append(v['phone'].strip())
+            person.append(v['email'].strip())
+
+        # print(phonebook_new.index(name))
+        phonebook_new.append(person)
+    return phonebook_new
+
+
 def write_contacts():
-    phonebook = insert_to_contact_list()
-    with open("phonebook.csv", "w", encoding="utf-8", newline="") as file:
+    phonebook = create_list(insert_to_contact_list())
+    with open("phonebook.csv", "w", encoding="utf-8", newline="\n") as file:
         datawriter = csv.writer(file, delimiter=",")
         datawriter.writerow(contact_list[0])
         datawriter.writerows(phonebook)
@@ -41,3 +59,6 @@ def write_contacts():
 
 write_contacts()
 # pprint(insert_to_contact_list())
+# phonebook2 = insert_to_contact_list()
+# print(phonebook2)
+# create_list(insert_to_contact_list())
